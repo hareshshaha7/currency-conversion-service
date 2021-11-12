bca7e26a28c1f73df45d8fdf893e144e8e218b22
Haresh Shaha <haresh.shaha7@gmail.com>
2021-11-12 14:30:00 +0530

    Invoking Currency Exchange from Currency Conversion Microservice

--- src/main/java/com/haresh/microservices/currencyconversionservice/CurrencyConversionController.java
+++ src/main/java/com/haresh/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -1,10 +1,13 @@
 package com.haresh.microservices.currencyconversionservice;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 public class CurrencyConversionController {
@@ -14,6 +17,22 @@ public class CurrencyConversionController {
             @PathVariable String from,
             @PathVariable String to,
             @PathVariable BigDecimal quantity) {
-        return new CurrencyConversion(10001L, from, to, BigDecimal.ONE, BigDecimal.TEN, BigDecimal.ZERO, "");
+//        return new CurrencyConversion(10001L, from, to, BigDecimal.ONE, BigDecimal.TEN, BigDecimal.ZERO, "");
+        HashMap<String, String> uriVariables = new HashMap<>();
+        uriVariables.put("from", from);
+        uriVariables.put("to", to);
+        ResponseEntity<CurrencyConversion> entity = new RestTemplate().getForEntity(
+                "http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+                CurrencyConversion.class,
+                uriVariables);
+
+        CurrencyConversion currencyConversion = entity.getBody();
+        System.out.println(currencyConversion);
+        if (currencyConversion == null) {
+            throw new RuntimeException("Unable to convert the currency value from " + from + " to " + to);
+        }
+        currencyConversion.setTotalCalculatedAmount(quantity.multiply(currencyConversion.getConversionMultiple()));
+
+        return currencyConversion;
     }
 }
